8ff684e54e64dc2c0780320ef6e9977af290eb00
Tianxiong He <kevin@Tianxiongs-Mac.local>
2016-09-20 14:02:57 -0700

    es6 Javascript

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
+import express from 'express'
+var app = express()
 
 app.get('/', function (req, res) {
   res.send('Initialize SFU-Commute backend server!');
-});
+})
 
 app.listen(3000, function () {
   console.log('Initialize SFU-Commute backend server!');
-});
+})
