6a146a288c01caa50a94c1584e06153f56df712c
kevinxh <kevintxhe933@hotmail.com>
2016-10-08 10:40:53 +0000

    4 digit code generation

--- server/router/authenticationRouter.js
+++ server/router/authenticationRouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { SignUp, SignIn } from '../controller/authenticationController'
+import { JWTAuthentication } from '../config/passport-jwt.js'
+import { SignUp, SignIn, Verify } from '../controller/authenticationController'
 
 const authenticationRouter = express.Router()
 
 authenticationRouter
   .post('/signup', SignUp)
   .post('/signin', SignIn)
+  .get('/verify', JWTAuthentication, Verify)
 
 export default authenticationRouter
