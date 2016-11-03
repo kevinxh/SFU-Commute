d80b79439e124a51990c224cb7fdb8e66fa0bd06
jyotsna jaswal <jyotsnajaswal.jyotsnaprofessional@gmail.com>
2016-11-03 12:00:32 -0700

    keyboardDismissed

--- SFUCommute/sfu_commute_V.2/SignupPage.swift
+++ SFUCommute/sfu_commute_V.2/SignupPage.swift
@@ -38,6 +38,10 @@ class SignupPage: UIViewController {
         lastName.text = "last name"
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
